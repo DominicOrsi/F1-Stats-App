fe51ceda4617842dbdad1f649e62d677fe8eba26
Dominic Orsi <73092665+DominicOrsi@users.noreply.github.com>
2023-01-15 15:25:32 -0800

    Delete Keys.kt

--- app/src/main/java/com/f1stats/greensector/Keys.kt
+++
@@ -1,3 +0,0 @@
-package com.f1stats.greensector
-
-const val SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRua25vd2drcG90enB4bWNwdmJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEyNDQ2OTksImV4cCI6MTk4NjgyMDY5OX0.RakVDrvPjI8nQaQ_aLXDcRaPFodeLR7Q2vvf9lb5Np0"
